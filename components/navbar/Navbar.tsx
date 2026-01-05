993f1e287a574781ab3b95538b2de101326ff2c5
alex100588 <alexandru_ionasc@yahoo.com>
2026-01-05 18:45:02 +0200

    modificari

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image"
 import Link from "next/link"
 import SearchFilters from "./SearchFilters"
+import UserNav from "./UserNav"
 
 export const Navbar = () => {
   return (
-    <nav className="w-full fixed top-0 left-0 py-2 border-b bg-white z-10">
+    <nav className="w-full fixed top-0 left-0 py-2 border-b bg-black z-10">
       <div className="max-w-[1500px] mx-auto px-6">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center ">
           <Link href="/">
             <Image
-              src="/logoAlex.png"
+              src="/logoAlex1.png"
               alt="Logo"
               width={120}
               height={40}
@@ -22,7 +23,7 @@ export const Navbar = () => {
           </div>
 
           <div className="flex items-center space-x-6">
-            Add property
+            <UserNav />
           </div>
         </div>
       </div>
--- components/navbar/SearchFilters.tsx
+++ components/navbar/SearchFilters.tsx
@@ -1,12 +1,34 @@
 import { cn } from "@/lib/utils"
 
 const itemClasses =
-  "h-[63px] px-8 flex flex-col justify-center rounded-full  hover:bg-red-100"
+  "h-[63px] px-8 flex flex-col cursor-pointer justify-center rounded-full hover:bg-gray-300"
 
 const SearchFilters = () => {
   return (
-    <div className="h-[64px] flex items-center justify-between border rounded-full">
-      <div className="flex items-center">
+    <div className="h-[64px] flex items-center border rounded-full px-2 bg-white">
+
+      {/* MOBILE */}
+      <div className="flex md:hidden items-center gap-2 px-4">
+        <div className="p-2 bg-gray-700 rounded-full text-white">
+          <svg
+            viewBox="0 0 32 32"
+            style={{
+              display: "block",
+              fill: "none",
+              height: 16,
+              width: 16,
+              stroke: "currentColor",
+              strokeWidth: 4,
+            }}
+          >
+            <path d="m20.666 20.666 10 10" />
+            <path d="m24.0002 12.6668c0 6.2593-5.0741 11.3334-11.3334 11.3334-6.2592 0-11.3333-5.0741-11.3333-11.3334 0-6.2592 5.0741-11.3333 11.3333-11.3333 6.2593 0 11.3334 5.0741 11.3334 11.3333z" />
+          </svg>
+        </div>
+      </div>
+
+      {/* DESKTOP */}
+      <div className="hidden md:flex items-center">
         <div className={cn(itemClasses)}>
           <p className="text-xs font-semibold">Where</p>
           <p className="text-sm">Wanted location</p>
@@ -28,10 +50,9 @@ const SearchFilters = () => {
         </div>
 
         <div className="p-2">
-          <div className="p-4 bg-gray-700 hover:bg-gray-500 transition rounded-full text-white">
+          <div className="p-4 cursor-pointer bg-gray-700 hover:bg-gray-500 transition rounded-full text-white">
             <svg
               viewBox="0 0 32 32"
-              aria-hidden="true"
               style={{
                 display: "block",
                 fill: "none",
@@ -39,7 +60,6 @@ const SearchFilters = () => {
                 width: 16,
                 stroke: "currentColor",
                 strokeWidth: 4,
-                overflow: "visible",
               }}
             >
               <path d="m20.666 20.666 10 10" />
